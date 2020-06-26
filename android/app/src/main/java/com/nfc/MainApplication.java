59e832495e664f595cdea390ef1050bd4eff0ac6
emad menkar <emadmenkar@gmail.com>
2020-06-26 21:05:02 +0300

    adjusting icon appear

--- android/app/src/main/java/com/nfc/MainApplication.java
+++ android/app/src/main/java/com/nfc/MainApplication.java
@@ -11,7 +11,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.horcrux.svg.SvgPackage;
+import com.horcrux.svg.SvgPackage; 
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
+          List<ReactPackage> packages = new PackageList(this).getPackages(); 
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new SvgPackage());
           return packages;
