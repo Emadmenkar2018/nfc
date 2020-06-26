30c0e4d5635dce2d59cee839f055ccf95c0ea5ee
emad menkar <emadmenkar@gmail.com>
2020-06-26 17:09:44 +0300

    adding day component

---
+++ components/foodSchedule/DayComponent.js
@@ -0,0 +1,25 @@
+
+
+import React from 'react';
+import { View,   StyleSheet , ,} from 'react-native';  
+import {_getDates,_extractDays,_extractDays2,_getDates2} from '../../utils/methods'  
+import { 
+    responsiveScreenFontSize
+  } from "react-native-responsive-dimensions"; 
+import DayComponent from './DayComponent'
+
+
+const DayComponent = ({   ...props }) => { 
+       return ( 
+           <View style={{width:'100%',height:'100%',alignItems:'center',paddingHorizontal:10,zIndex:0}}>
+                
+           </View>
+       ) 
+   } 
+
+export default DayComponent;
+
+
+const styles = StyleSheet.create({ 
+   
+ });
