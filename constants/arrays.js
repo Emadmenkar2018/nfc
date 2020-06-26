b1fbc629fe674f2eee0230c14fd036d389542951
emad menkar <emadmenkar@gmail.com>
2020-06-26 16:55:19 +0300

    adding arrays

---
+++ constants/arrays.js
@@ -0,0 +1,206 @@
+
+  export const _getdayofWeek=['Pzt','Sal','Çar','Per','Cum','Cmt','Paz']
+
+  export const IMAGES = {
+    body: require('../assets/body.png'),  
+    hand: require('../assets/hand.png'),  
+    belly: require('../assets/belly.png'),  
+    Metabolizm: require('../assets/metabolism.png'),  
+    Energy: require('../assets/energy.png'),  
+    Fizik: require('../assets/runner.png'),  
+    exercise: require('../assets/stopwatch.png'),  
+    fat: require('../assets/cals.png'),  
+    sleep: require('../assets/half-moon.png'),  
+    add :require('../assets/plus.png'),
+    ruler : require('../assets/ruler.png'),
+    arrow : require('../assets/right-arrow.png'),
+    hedef : require('../assets/target.png'),
+    heart : require('../assets/heart.png'),
+    calendar : require('../assets/calendar.png'),
+    edit : require('../assets/edit.png'),
+    delete : require('../assets/delete.png'),
+    ozelcalender : require('../assets/ozelcalender.png'),
+    cross : require('../assets/cross.png'),
+  } 
+
+//   export const mealsData = {
+//     "2020-03-02": {
+//         "user_breakfasts": [],
+//         "user_lunches": [{
+//             "id": 1,
+//             "user_id": 23,
+//             "content": "tavuklu pilav nohutlu",
+//             "created_at": "2020-03-03 00:00:00",
+//             "updated_at": "2020-02-20 11:53:39"
+//         }],
+//         "user_dinners": [],
+//         "user_exercises": [],
+//         "user_waters": [
+//             {
+//                 "id": 1,
+//                 "user_id": 23,
+//                 "content": 24,
+//                 "created_at": "2020-03-02 00:00:00",
+//                 "updated_at": "2020-03-02 10:56:06"
+//             }
+//         ],
+//         "user_apperatives": [],
+//         "user_sleeps": []
+//     },
+//     "2020-03-03": {
+//         "user_breakfasts": [{
+//             "id": 3,
+//             "user_id": 23,
+//             "content": "dasdaddad",
+//             "created_at": "2020-03-02 00:00:00",
+//             "updated_at": "2020-03-02 10:30:22",
+//             "deleted_at": null
+//         }],
+//         "user_lunches": [
+//             {
+//                 "id": 1,
+//                 "user_id": 23,
+//                 "content": "tavuklu pilav nohutlu",
+//                 "created_at": "2020-03-03 00:00:00",
+//                 "updated_at": "2020-02-20 11:53:39"
+//             }
+//         ],
+//         "user_dinners": [],
+//         "user_exercises": [],
+//         "user_waters": [],
+//         "user_apperatives": [],
+//         "user_sleeps": []
+//     },
+//     "2020-03-04": {
+//         "user_breakfasts": [{
+//             "id": 3,
+//             "user_id": 23,
+//             "content": "dasdaddad",
+//             "created_at": "2020-03-02 00:00:00",
+//             "updated_at": "2020-03-02 10:30:22",
+//             "deleted_at": null
+//         }],
+//         "user_lunches": [{
+//             "id": 1,
+//             "user_id": 23,
+//             "content": "tavuklu pilav nohutlu",
+//             "created_at": "2020-03-03 00:00:00",
+//             "updated_at": "2020-02-20 11:53:39"
+//         }],
+//         "user_dinners": [],
+//         "user_exercises": [],
+//         "user_waters": [],
+//         "user_apperatives": [],
+//         "user_sleeps": []
+//     },
+//     "2020-03-05": {
+//         "user_breakfasts": [],
+//         "user_lunches": [{
+//             "id": 1,
+//             "user_id": 23,
+//             "content": "tavuklu pilav nohutlu",
+//             "created_at": "2020-03-03 00:00:00",
+//             "updated_at": "2020-02-20 11:53:39"
+//         }],
+//         "user_dinners": [{
+//             "id": 1,
+//             "user_id": 23,
+//             "content": "tavuklu pilav nohutlu",
+//             "created_at": "2020-03-03 00:00:00",
+//             "updated_at": "2020-02-20 11:53:39"
+//         }],
+//         "user_exercises": [],
+//         "user_waters": [],
+//         "user_apperatives": [],
+//         "user_sleeps": []
+//     },
+//     "2020-03-06": {
+//         "user_breakfasts": [{
+//             "id": 3,
+//             "user_id": 23,
+//             "content": "dasdaddad",
+//             "created_at": "2020-03-02 00:00:00",
+//             "updated_at": "2020-03-02 10:30:22",
+//             "deleted_at": null
+//         }],
+//         "user_lunches": [{
+//             "id": 1,
+//             "user_id": 23,
+//             "content": "tavuklu pilav nohutlu",
+//             "created_at": "2020-03-03 00:00:00",
+//             "updated_at": "2020-02-20 11:53:39"
+//         }],
+//         "user_dinners": [{
+//             "id": 1,
+//             "user_id": 23,
+//             "content": "tavuklu pilav nohutlu",
+//             "created_at": "2020-03-03 00:00:00",
+//             "updated_at": "2020-02-20 11:53:39"
+//         }],
+//         "user_exercises": [{
+//             "id": 1,
+//             "user_id": 23,
+//             "content": "tavuklu pilav nohutlu",
+//             "created_at": "2020-03-03 00:00:00",
+//             "updated_at": "2020-02-20 11:53:39"
+//         }],
+//         "user_waters": [],
+//         "user_apperatives": [],
+//         "user_sleeps": [{
+//             "id": 1,
+//             "user_id": 23,
+//             "content": "tavuklu pilav nohutlu",
+//             "created_at": "2020-03-03 00:00:00",
+//             "updated_at": "2020-02-20 11:53:39"
+//         }]
+//     },
+//     "2020-03-07": {
+//         "user_breakfasts": [
+//             {
+//                 "id": 2,
+//                 "user_id": 23,
+//                 "content": "dasda",
+//                 "created_at": "2020-03-07 00:00:00",
+//                 "updated_at": "2020-02-21 12:57:05",
+//                 "deleted_at": null
+//             }
+//         ],
+//         "user_lunches": [ {
+//             "id": 2,
+//             "user_id": 23,
+//             "content": "dasda",
+//             "created_at": "2020-03-07 00:00:00",
+//             "updated_at": "2020-02-21 12:57:05",
+//             "deleted_at": null
+//         }],
+//         "user_dinners": [{
+//             "id": 1,
+//             "user_id": 23,
+//             "content": "tavuklu pilav nohutlu",
+//             "created_at": "2020-03-03 00:00:00",
+//             "updated_at": "2020-02-20 11:53:39"
+//         }],
+//         "user_exercises": [],
+//         "user_waters": [],
+//         "user_apperatives": [],
+//         "user_sleeps": []
+//     },
+//     "2020-03-08": {
+//         "user_breakfasts": [ ],
+//         "user_lunches": [],
+//         "user_dinners": [],
+//         "user_exercises": [],
+//         "user_waters": [],
+//         "user_apperatives": [],
+//         "user_sleeps": []
+//     },
+//     "2020-03-09": {
+//         "user_breakfasts": [],
+//         "user_lunches": [],
+//         "user_dinners": [],
+//         "user_exercises": [],
+//         "user_waters": [],
+//         "user_apperatives": [],
+//         "user_sleeps": []
+//     }
+// }
