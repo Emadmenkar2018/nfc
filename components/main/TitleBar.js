5d9198df6ba37c8991701602064970781dae2324
emad menkar <emadmenkar@gmail.com>
2020-06-29 10:32:18 +0300

    adding title bar

---
+++ components/main/TitleBar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {View ,Image ,Text } from 'react-native'
+import MainTab from './MainTab'
+import {Svg ,Rect ,Circle} from 'react-native-svg';
+import { Icon } from 'react-native-elements';
+
+
+const TitleBar = (props) =>{
+    return (
+        <View style={{height:'8%', width:'100%' ,marginTop:6 , marginBottom:0  }} >
+
+            <View style={{backgroundColor:'#E9ECF1',height:'98%', width:'100%', flexDirection:'row', justifyContent:'space-between', alignItems:'center' , paddingHorizontal:15}}>
+
+                <Icon name="bars" type="font-awesome" color="#253545"/>
+
+                <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}> 
+                    <Image style={{width: 30, height: 30}}    tintColor={'#FF6F00'} source={require('../../assets/doctor.png')} resizeMode="contain"/>
+
+                    <Text style={{fontFamily:'Pacifico-Regular'}}>MyApp</Text>
+                </View>
+
+
+                <Icon name="user" type="font-awesome" color="#253545"/>
+
+
+            </View>
+ 
+
+        </View>
+     
+
+            
+    )
+}
+
+export default TitleBar
