fed85076c08ee326503ba2bed06610b9cc838774
emad menkar <emadmenkar@gmail.com>
2020-07-01 13:25:38 +0300

    transition fixed with flux

--- components/log/InqueryComp.js
+++ components/log/InqueryComp.js
@@ -2,10 +2,11 @@ import React from 'react'
 import {View ,Text} from 'react-native'
 import { responsiveScreenFontSize } from 'react-native-responsive-dimensions';
 import {Icon} from 'react-native-elements' 
+import { Actions } from 'react-native-router-flux';
 
 
-const InqueryComp = (props) =>{
-
+const InqueryComp = (props) =>{ 
+ 
     // let history = useHistory();
     return(
         <View style={{width:'90%',backgroundColor:'#fff',marginVertical:10,alignSelf:'center',padding:15,borderRadius:10,justifyContent:'space-between',flexDirection:'row'}}>
@@ -20,7 +21,7 @@ const InqueryComp = (props) =>{
 
                 <Icon name="save" type="material" containerStyle={{marginRight:10}} size={responsiveScreenFontSize(2.5)} color={'#273444'} />
 
-                <Icon name="keyboard-arrow-right"  onPress={()=>history.push('/Main/Table')}  type="material" size={responsiveScreenFontSize(2.5)} color={'#273444'} />
+                <Icon name="keyboard-arrow-right"  onPress={()=>Actions.push('table') }  type="material" size={responsiveScreenFontSize(2.5)} color={'#273444'} />
 
             </View>
 
