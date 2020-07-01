a27774d0220fdb146641f8349d8004cf789ab5b0
emad menkar <emadmenkar@gmail.com>
2020-07-01 13:24:37 +0300

    table screen edited with colors

--- src/screens/TableScreen.js
+++ src/screens/TableScreen.js
@@ -1,63 +1,65 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
-import { Table, TableWrapper,Col, Cols, Cell } from 'react-native-table-component';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { Table, TableWrapper, Row } from 'react-native-table-component';
+import TableHeader from '../../components/table/TableHeader'
  
-export default class TableScreen extends Component {
+export default class ExampleThree extends Component {
   constructor(props) {
     super(props);
-    const elementButton = (value) => (
-      <TouchableOpacity onPress={() => this._alertIndex(value)}>
-        <View style={styles.btn}>
-        <Text style={styles.btnText}>{value}</Text>
-        </View>
-      </TouchableOpacity>
-    );
- 
     this.state = {
-      tableTitle: ['Title', 'Title2', 'Title3', 'Title4'],
-      tableData: [
-        [elementButton('mg_per_dl'), 'a', 'b', 'c', 'd'],
-        [elementButton('mmol_per_liter'), '1', '2', '3', '4'],
-        [elementButton('mg_per_dl_corrected_with_custom_formula'), 'a', 'b', 'c', 'd']
-      ]
+      tableHead: ['DATE', 'TIME', 'mg_per_dl', 'mmol_per_liter', 'mg_per_dl_corrected_with_custom_formula' ],
+      widthArr: [40, 60, 80, 100, 150 ]
     }
-  }
- 
-  _alertIndex(value) {
-    Alert.alert(`This is column ${value}`);
   }
  
   render() {
     const state = this.state;
+    const tableData = [];
+    for (let i = 0; i < 10; i += 1) {
+      const rowData = [];
+      for (let j = 0; j < 9; j += 1) {
+        rowData.push(`${i}${j}`);
+      }
+      tableData.push(rowData);
+    }
+ 
     return (
       <View style={styles.container}>
-        <Table style={{flexDirection: 'row'}} borderStyle={{borderWidth: 1}}>
-          {/* Left Wrapper */}
-          <TableWrapper style={{width: 80}}>
-            <Cell data="" style={styles.singleHead}/>
-            <TableWrapper style={{flexDirection: 'row'}}>
-              <Col data={['H1', 'H2']} style={styles.head} heightArr={[60, 60]} textStyle={styles.text} />
-              <Col data={state.tableTitle} style={styles.title} heightArr={[30, 30, 30, 30]} textStyle={styles.titleText}></Col>
-            </TableWrapper>
-          </TableWrapper>
+      
+        <TableHeader/>
  
-          {/* Right Wrapper */}
-          <TableWrapper style={{flex:1}}>
-            <Cols data={state.tableData} heightArr={[80, 30, 30, 30, 30]} textStyle={styles.text}/>
-          </TableWrapper>
-        </Table>
+
+        <ScrollView style={{}} horizontal={true}>
+          <View>
+            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+              <Row data={state.tableHead} widthArr={state.widthArr} style={styles.header} textStyle={styles.text}/>
+            </Table>
+            <ScrollView style={styles.dataWrapper}>
+              <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+                {
+                  tableData.map((rowData, index) => (
+                    <Row
+                      key={index}
+                      data={rowData}
+                      widthArr={state.widthArr}
+                      style={[styles.row, index%2 && {backgroundColor: 'rgba(255, 111, 0,0.2)'}]}
+                      textStyle={styles.text}
+                    />
+                  ))
+                }
+              </Table>
+            </ScrollView>
+          </View>
+        </ScrollView>
       </View>
     )
   }
 }
  
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-  singleHead: { width: 80, height: 80, backgroundColor: '#c8e1ff' },
-  head: { flex: 1, backgroundColor: '#c8e1ff' },
-  title: { flex: 2, backgroundColor: '#f6f8fa' },
-  titleText: { marginRight: 6, textAlign:'right' },
-  text: { textAlign: 'center' },
-  btn: { width: 58, height: 18, marginLeft: 15, borderRadius: 2 },
-  btnText: { textAlign: 'center' }
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#f4f4f4', },
+  header: { height: 50, backgroundColor: 'rgba(255, 111, 0,0.8)',  },
+  text: { textAlign: 'center', fontWeight: '100',fontFamily:'BarlowCondensed-Medium' },
+  dataWrapper: { marginTop: -1 },
+  row: { height: 40, backgroundColor: '#fff' }
 });
