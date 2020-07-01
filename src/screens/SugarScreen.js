ef44fe1199d7c7a8c63fb65de42587bd34241ec0
emad menkar <emadmenkar@gmail.com>
2020-07-01 13:25:16 +0300

    added chart

--- src/screens/SugarScreen.js
+++ src/screens/SugarScreen.js
@@ -1,25 +1,79 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import {View, ImageBackground , StyleSheet} from 'react-native'
 import { Tile ,Button,Text } from 'react-native-elements';
 import { responsiveScreenFontSize } from 'react-native-responsive-dimensions'; 
+import { Actions } from 'react-native-router-flux';
+import { LineChart,   } from "react-native-chart-kit";
+import { Dimensions } from "react-native"; 
+import SugarModal  from '../../components/sugar/SugarModal'
+
+const screenWidth = Dimensions.get("window").width;
 
 const SugarScreen =({ history, ...props })   =>{
+
+   const [modalVisibility, setModalVisibility] = useState(false)
+
+      
  
+    const chartConfig = {
+        backgroundGradientFrom: "#F4F4F4",
+        // backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: "#F4F4F4",
+        // backgroundGradientToOpacity: 0, 
+        color: (opacity = 1) => `rgba(15,21,27, ${opacity})`, // optional
+        strokeWidth: 2, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: false, // optional
+        fillShadowGradient : '#fff'	
+      };
+
+    const data = {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+          {
+            data: [20, 45, 28, 80, 99, 43], 
+            color: (opacity = 1) => `rgba(255, 111, 0, ${opacity+0.2})`, // optional
+            strokeWidth: 2 // optional
+          }
+        ], 
+      };
+
+
+      const closeModal = ()=>{
+        setModalVisibility(false)
+      }
+
     return (
         // <View> 
-        <View style={{justifyContent:'flex-start'}}>
+        <View style={{flex:1,justifyContent:'flex-start',backgroundColor:'#f4f4f4'}}>
 
 
-            <View style={{height:'70%',width:'100%'}}>
+            <View style={{height:'45%',width:'100%',marginBottom:10,color:'rgba(15,21,27,1)'}}>
+
+                <Text style={{alignSelf:'center',marginBottom:10,fontFamily:'BarlowCondensed-Bold'}}>Gelişme Grafiği</Text>
+ 
+                <LineChart
+                    data={data}
+                    width={screenWidth}
+                    height={256} 
+                    chartConfig={chartConfig}
+                    withDots={false}
+                    withInnerLines={false}
+                    // bezier
+                    />
+            </View>
+
+
+             <View style={{height:'30%',width:'100%',marginTop:'auto',marginBottom:20,elevation:2 ,borderRadius:10}}>
 
                 <ImageBackground resizeMode={'cover'}  style={{width:'100%' ,borderRadius:10,marginBottom:0,backgroundColor:'#000'}} source={require('../../assets/check.jpg')}>
-                    <View style={{width:'100%',height:'100%',backgroundColor:'rgba(0,0,0,.3)',justifyContent:'flex-end',alignItems:'flex-start',paddingLeft:20,borderRadius:10}}>
+                    <View style={{width:'100%',height:'100%',backgroundColor:'rgba(0,0,0,.5)',justifyContent:'flex-end',alignItems:'flex-start',paddingLeft:20,borderRadius:10}}>
 
                         <Text style={{fontSize:25,color:'#fff',marginBottom:10,fontFamily:'BarlowCondensed-Regular'}}>Bugün Taramayı Yaptın Mı ?</Text>
 
 
                         <Button 
-                        onPress={()=>history.push("/Main/NfcReader")}
+                        onPress={()=>setModalVisibility(true)}
                         title="Tara Şimdi" 
                         containerStyle={{borderRadius:20  ,marginBottom:15 }}
                         buttonStyle={{ backgroundColor:'#FF6F00'}}
@@ -30,8 +84,16 @@ const SugarScreen =({ history, ...props })   =>{
 
                 </ImageBackground> 
 
-            </View>
+            </View> 
 
+
+            <SugarModal
+              closeModal={closeModal}
+              modalVisibility={modalVisibility}
+              />
+
+            
+{/* 
             <View style={styles.talkBubble}>
                 <View style={styles.talkBubbleSquare}>
 
@@ -50,7 +112,7 @@ const SugarScreen =({ history, ...props })   =>{
                 <Text style={{fontSize:responsiveScreenFontSize(1.9),color:'#000', fontFamily:'BarlowCondensed-Regular'}}>Karşılaştığınız sorunu içeren bir e-posta gönderin</Text>
                 <Text style={{fontSize:responsiveScreenFontSize(1.9),color:'#263645', fontFamily:'BarlowCondensed-Regular'}}>emadmenkar@gmail.com</Text>
 
-            </View>
+            </View> */}
 
         </View>
 
