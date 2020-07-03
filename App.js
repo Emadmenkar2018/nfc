c988ab7fd7a89764652f6e40ef137fab34ed24c8
emad menkar <emadmenkar@gmail.com>
2020-07-03 11:34:11 +0300

    making the auth secure

--- App.js
+++ App.js
@@ -1,61 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState ,useEffect} from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native'; 
-import HomeScreen from './src/screens/HomeScreen'   
+import HomeScreen from './src/screens/HomeScreen'    
 import NfcReaderScreen from './src/screens/NfcReaderScreen'   
 import TableScreen from './src/screens/TableScreen'    
 import { Router, Scene } from 'react-native-router-flux'
+import auth   from '@react-native-firebase/auth';
+import LoginScreen from './src/screens/auth/LoginScreen'
+import RegisterScreen from './src/screens/auth/RegisterScreen'
+import database from '@react-native-firebase/database';
 
-// import {Provider} from 'react-redux';
+const App = ()=>{
 
   
-// const store = initStore(); 
 
+  const config = { 
+    apiKey :"AIzaSyBBfGNm7-oRvajx8mtuC075G83qs98IZ8E",
+    authDomain : ""
+  }
+  
 
-export default class App extends Component {
-  render() {
-    return ( 
-        // <NativeRouter>
-        //   <Switch>
-        //   <Route  
-        //       exact path="/" 
-        //       render={props => {
-        //         return <HomeScreen
-        //           {...props}
-        //         />
-        //       }}
-        //     /> 
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
-        //     <Route  
-        //       exact path="/Main/NfcReader" 
-        //       render={props => {
-        //         return <NfcReaderScreen
-        //           {...props}
-        //         />
-        //       }}
-        //     /> 
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+  
+  useEffect(() => {
+    const subscriber =  auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
 
-            
-        //     <Route  
-        //       exact path="/Main/Table" 
-        //       render={props => {
-        //         return <TableScreen
-        //           {...props}
-        //         />
-        //       }}
-        //     /> 
+  if (initializing) return null;
 
-        //   </Switch>
-        // </NativeRouter> 
-        <Router>
-        <Scene key = "root">
-           <Scene key = "home" component = {HomeScreen}  hideNavBar={true} title = "Home" initial = {true} />
-           <Scene key = "nfcReader" component = {NfcReaderScreen} hideNavBar={true}  title = "NfcReader" />
-           <Scene key = "table" component = {TableScreen} hideNavBar={true}   title = "Table" />
+  if (!user) {
+    return (
+      <Router>
+        <Scene key = "auth">
+           <Scene key = "login" component = {LoginScreen}  hideNavBar={true} title = "Login" initial = {true} /> 
+           <Scene key = "register" component = {RegisterScreen}  hideNavBar={true} title = "Register"   /> 
         </Scene>
       </Router>
-      
     );
   }
+
+  else { 
+
+    return (  
+           <Router>
+              <Scene key = "root">
+                  <Scene key = "home" component = {HomeScreen}  hideNavBar={true} title = "Home" initial = {true} />
+                  <Scene key = "nfcReader" component = {NfcReaderScreen} hideNavBar={true}  title = "NfcReader" />
+                  <Scene key = "table" component = {TableScreen} hideNavBar={true}   title = "Table" />
+              </Scene>
+          </Router>
+      ) 
+
+    } 
 }
 
 const styles = StyleSheet.create({
@@ -66,3 +68,5 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
   },
 });
+
+export default App
