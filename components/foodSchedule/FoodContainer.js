08e21bec038a7a76e23b29145958e5da35330d06
emad menkar <emadmenkar@gmail.com>
2020-07-07 14:10:42 +0300

    centralized the icons

--- components/foodSchedule/FoodContainer.js
+++ components/foodSchedule/FoodContainer.js
@@ -16,32 +16,33 @@ const FoodContainer = ({   ...props }) => {
 
                 <View style={{...styles.container }}>
 
-                    <View style={{flexDirection:'row',alignItems:'center'}}>
+                    <View>
 
-                        <Image style={{width:20,height:20,marginHorizontal:10}} resizeMode={'contain'} tintColor={'#000'} source={props.title.includes('kahval') ?  require('../../assets/bread.png') : props.title.includes('ana') ? require('../../assets/lunch.png') :    require('../../assets/dinner.png') }/>                    
-                        <Text style={{color:'#263645',fontSize:responsiveScreenFontSize(2.2),fontFamily:'BarlowCondensed-SemiBold'  }}>{props.title}</Text>
-                    
-                    </View>
+                        <View style={{flexDirection:'row',alignItems:'center'}}>
+
+                            <Image style={{width:20,height:20,marginHorizontal:10}} resizeMode={'contain'} tintColor={'#000'} source={props.title.includes('Kahvaltı') ?  require('../../assets/bread.png') : props.title.includes('Ana') ? require('../../assets/lunch.png') :    require('../../assets/dinner.png') }/>                    
+                            <Text style={{color:'#263645',fontSize:responsiveScreenFontSize(2.2),fontFamily:'BarlowCondensed-SemiBold'  }}>{props.title}</Text>
                         
-                   
-                  
-                   <View style={{width:'100%',flexDirection:'row', justifyContent:'space-between',marginVertical:15}}>
-
-                        <Text style={{width : '60%',color:'#888',fontSize:responsiveScreenFontSize(2),fontFamily:'BarlowCondensed-Regular',alignSelf:'center'}}>Yumurta Peynir Ekmek </Text>
-                        
-                        {/* <Text style={{color:'#263645',fontSize:responsiveScreenFontSize(2),fontFamily:'BarlowCondensed-Regular',alignSelf:'center'}}>{props.title}</Text> */}
-            
-                        <View style={{alignitems:'center' , flexDirection:'row'}}>
-
-                            <Icon name ="edit" containerStyle={{marginHorizontal:5}} type="material" size={responsiveScreenFontSize(2)} />
-
-                            <Icon name ="delete" containerStyle={{marginHorizontal:5}} type="material" size={responsiveScreenFontSize(2)} />
-
-
                         </View>
+                            
+                    
+                    
+                        <View style={{width:'100%',flexDirection:'row', justifyContent:'space-between',marginVertical:15}}>
 
-                   </View> 
-               
+                                <Text style={{width : '60%',color:'#888',fontSize:responsiveScreenFontSize(2),fontFamily:'BarlowCondensed-Regular',alignSelf:'center'}}>{props.content}</Text>
+                                
+                        </View> 
+
+                    </View>
+
+
+                   <View style={{alignitems:'center' , flexDirection:'row'}}> 
+
+                        {/* <Icon name ="edit" containerStyle={{marginHorizontal:5}} type="material" size={responsiveScreenFontSize(2)} /> */}
+
+                        <Icon name ="delete" containerStyle={{marginHorizontal:5}} type="material" size={responsiveScreenFontSize(2)} />
+
+                    </View>
                 
                 </View>
 
@@ -54,18 +55,21 @@ export default FoodContainer;
 
 const styles = StyleSheet.create({ 
    container : {
-    elevation: 2 ,
+    elevation: 1 ,
     alignSelf:'center', 
     width : '90%',
     marginHorizontal:5,   
     marginVertical:10 , 
     // backgroundColor:'#E9ECF1', 
     backgroundColor:'#fff', 
-    borderRadius:6,
+    borderRadius:10,
     paddingHorizontal:10, 
     paddingVertical:5,
-    borderLeftWidth:15,
-    borderLeftColor:'#fe796d'
+    borderLeftWidth:10,
+    borderLeftColor:'#fe796d',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center'
     
    }
  });
