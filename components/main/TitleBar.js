2e4a753b2ade57841dc3caddda7a370e700d17db
emad menkar <emadmenkar@gmail.com>
2020-07-07 10:35:00 +0300

    changing title in main screen

--- components/main/TitleBar.js
+++ components/main/TitleBar.js
@@ -4,6 +4,7 @@ import MainTab from './MainTab'
 import {Svg ,Rect ,Circle} from 'react-native-svg';
 import { Icon } from 'react-native-elements';
 import auth   from '@react-native-firebase/auth';
+import { responsiveWidth } from 'react-native-responsive-dimensions';
 
 const TitleBar = (props) =>{
 
@@ -15,14 +16,13 @@ const TitleBar = (props) =>{
     return (
         <View style={{height:'8%', width:'100%' ,marginTop:6 , marginBottom:0 ,borderBottomWidth:1,borderBottomColor:'#555' }} >
 
-            <View style={{backgroundColor:'#E9ECF1',height:'98%', width:'100%', flexDirection:'row', justifyContent:'space-between', alignItems:'center' , paddingHorizontal:15}}>
+            <View style={{backgroundColor:'#fff5f2',height:'98%', width:'100%', flexDirection:'row', justifyContent:'space-between', alignItems:'center' , paddingHorizontal:15}}>
 
                 <Icon onPress={onLogout} name="bars" type="font-awesome" color="#253545"/>
 
                 <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}> 
-                    <Image style={{width: 30, height: 30}}    tintColor={'#FF6F00'} source={require('../../assets/doctor.png')} resizeMode="contain"/>
-
-                    <Text style={{fontFamily:'Pacifico-Regular'}}>Analyser</Text>
+                    <Image style={{width: responsiveWidth(30), height: responsiveWidth(15)}}     source={require('../../assets/logoTra.png')} resizeMode="contain"/>
+ 
                 </View>
 
 
